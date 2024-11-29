34be66bca53b7266ba775f7ec31cd67332c4286a
Bracken <BrackenLo@proton.me>
2024-11-29 17:49:54 +0000

    Core

---
+++ roots_core/src/lib.rs
@@ -0,0 +1,10 @@
+//====================================================================
+
+pub use roots_common as common;
+pub use roots_pipelines as pipelines;
+pub use roots_renderer as renderer;
+pub use roots_text as text;
+
+//====================================================================
+
+//====================================================================
